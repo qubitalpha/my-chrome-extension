54fb7c498bd321afd6bcd84d24240fb31c6e86ec
Qubit Alpha <44535606+qubitalpha@users.noreply.github.com>
2024-08-10 18:37:43 -0700

    Connects HTML and JS.

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,9 @@
 // You can add functionality for your popup here
-   console.log("Popup script loaded 1.0");
+   console.log("Popup from JS script 2.0");
+
+   // popup.js
+document.addEventListener('DOMContentLoaded', () => {
+   const outputDiv = document.getElementById('output');
+   // Your JavaScript logic to generate output
+   outputDiv.textContent = 'Hello from JS script!';
+ });
